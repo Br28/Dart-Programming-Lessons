3d0881829b7c42da5bfedbdf7e691a243d015a2a
Bruno Simachulu <94797356+Br28@users.noreply.github.com>
2023-03-06 15:31:59 +0200

    Started work 3

---
+++ The Lessons/3. Work/inference.dart
@@ -0,0 +1,14 @@
+void main() {
+  // var The var keyword can hold any kind of datatype after intialization
+  var name = 100;
+
+  // final The final keyword holds a constant value at run time
+  final grade = 70;
+
+  // Const The const keyword holds a constant value at compile time
+  const height = 1.72;
+
+  // dynamic The dynamic keyword can hold a value that can be changed at any time
+  dynamic range = 1;
+  range = "so far";
+}
